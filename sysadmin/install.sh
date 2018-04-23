254c0a114c4239ee28af4d2378d1ae6117515ec7
Peter Xu <px.peter.xu@gmail.com>
2018-04-22 23:22:56 -0700

    Add install scripts

---
+++ sysadmin/install.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#
+# Prompts
+#
+
+NODE_SERVER_PORT=''  # SET THIS!
+if [[ -z "$NODE_SERVER_PORT" ]]; then
+  echo 'Please set NODE_SERVER_PORT in install.sh, then remove this check'
+end
+
+MYSQL_ROOT_PASSWORD=''
+echo 'Please enter the MySQL root user password'
+read -e MYSQL_ROOT_PASSWORD
+
+REDIS_PASSWORD=''
+echo 'Please enter the Redis password'
+read -e REDIS_PASSWORD
+
+DOMAIN=''
+echo 'Please enter the domain to use (PHPMyAdmin will be at thisispma.DOMAIN)'
+read -e DOMAIN
+
+cd $DIR
+
+# Main server
+bash install-scripts/components/nodejs.sh
+bash install-scripts/components/yarn.sh
+bash install-scripts/components/nginx.sh
+bash install-scripts/components/certificates.sh "$DOMAIN" "$NODE_SERVER_PORT"
+
+# MySQL/MariaDB
+bash install-scripts/components/mariadb.sh "$MYSQL_ROOT_PASSWORD"
+sed -i 's/(bind-address\s*=\s*)127.0.0.1/\10.0.0.0/' /etc/mysql/my.cnf
+systemctl restart mariadb
+
+# Setup phpMyAdmin
+bash install-scripts/components/phpmyadmin.sh
+mv -f /etc/nginx/sites-available/phpmyadmin /etc/nginx/sites-available/phpmyadmin-ssl
+cp -f install-scripts/nginx/initial-single /etc/nginx/sites-available/phpmyadmin
+systemctl reload nginx
+bash install-scripts/components/letsencrypt.sh "thisispma.$DOMAIN"
+mv -f /etc/nginx/sites-available/phpmyadmin-ssl /etc/nginx/sites-available/phpmyadmin
+sed -i "s/thisispma\.DOMAIN\.com/thisispma.$DOMAIN/g" /etc/nginx/sites-available/phpmyadmin
+systemctl reload nginx
+
+# Others
+bash install-scripts/components/logrotate.sh
+bash install-scripts/components/redis.sh "$REDIS_PASSWORD"
---
+++ sysadmin/preinstall.sh
@@ -0,0 +1,39 @@
+#
+# Before using this file, make sure you fill in:
+# - YOUR_REPOSITORY
+#
+
+cat > preinstall.sh <<'END'
+#!/bin/sh
+
+#
+# This script should be downloaded and run before running all
+# other install scripts
+#
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+  ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
+fi
+
+KEY_ADDED=""
+while [ "$KEY_ADDED" != "y" ]; do
+  cat ~/.ssh/id_rsa.pub
+  echo ''
+  echo 'Please add the above key to GitLab as a deployment key'
+  echo 'for both this project and install-scripts'
+  echo 'so that this server can pull the code, and then enter y'
+  read -e KEY_ADDED
+done
+
+# Set up user and /home/web directory
+NODE_WEB_PASS=$(</dev/urandom tr -dc 'A-Za-z0-9!"#$%&'\''()*+,-./:;<=>?@[\]^_`{|}~' | head -c 32 ; echo)
+useradd --home /home/web --shell /bin/bash --create-home web
+echo "node-web:$NODE_WEB_PASS" | chpasswd
+
+# Clone the code
+apt-get install -y git
+cd /home/web
+git clone git@gitlab.com:myrtlelime/YOUR_REPOSITORY.git app
+cd app/sysadmin
+git clone git@gitlab.com:myrtlelime/install-scripts.git
+END
